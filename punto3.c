84751463791fd932a2fad7795533385a2c48fcba
Jorge Donaire <jorgedonaire93@gmail.com>
2023-04-24 01:19:52 -0300

    Creacion de nodos y listas

--- punto3.c
+++ punto3.c
@@ -1,50 +1,73 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
-struct Tarea
+typedef struct Tarea
 {
     int TareaID;
     char *Descripcion;
     int Duracion;
-}typedef Tarea;
+}Tarea;
 
-struct Nodo
+typedef struct Nodo
 {
     Tarea T;
-    Nodo *Siguiente;
-}typedef Nodo;
+    struct Nodo* Siguiente;
+}Nodo;
 
 Nodo *CrearListaVacia();
-Nodo *CrearNodo(Tarea NuevaTarea);
-void InsertarNodo(Nodo **Start, Tarea NuevaTarea);
+Nodo *CrearNodo();
+void InsertarNodo(Nodo **Start);
+void mostrarLista(Nodo *Start);
+void EliminarNodo(Nodo * nodo);
 
 
 int main(){
-    Nodo * Start;
-    Start = CrearListaVacia(); 
+    Nodo * StartTareasPendientes;
+    Nodo * StartTareasRealizadas;
 
+    StartTareasPendientes = CrearListaVacia(); 
+    StartTareasRealizadas = CrearListaVacia();
 
+    InsertarNodo(&StartTareasPendientes);
+    mostrarLista(StartTareasPendientes);
+    
+
+    return 0;
 }
 
 Nodo *CrearListaVacia(){
     return NULL;
 }
 
-Nodo *CrearNodo(Tarea NuevaTarea){
+Nodo *CrearNodo(){
     srand(time(NULL));
     Nodo *NuevoNodo = (Nodo *) malloc(sizeof(Nodo));
     NuevoNodo->T.Descripcion = "DESCRIPCION";
     NuevoNodo->T.Duracion=10+rand()%101;
+    NuevoNodo->T.TareaID=69;
     NuevoNodo->Siguiente=NULL;
     return NuevoNodo;
 }
 
-void InsertarNodo(Nodo **Start, Tarea NuevaTarea){
-    Nodo *NuevoNodo = CrearNodo(NuevaTarea);
+void InsertarNodo(Nodo **Start){
+    Nodo *NuevoNodo = CrearNodo();
     NuevoNodo->Siguiente=*Start;
     *Start = NuevoNodo;
 }
 
+void mostrarLista(Nodo *Start){
+    Nodo *temp = Start;
+    while (temp != NULL)
+    {
+        printf("Descripcion: %s\n", temp->T.Descripcion);
+        printf("Duracion: %d\n", temp->T.Duracion);
+        printf("Tarea ID: %d\n", temp->T.TareaID);
+
+        temp=temp->Siguiente;
+    }
+    
+}
 
 void EliminarNodo(Nodo * nodo){
     if (!nodo)
